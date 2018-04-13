244414ee643a8ed4f2bfc130fb9577392a1380c5
Your Name <741916757@qq.com>
2018-04-13 21:03:07 +0800

    dala

---
+++ js/mypage.js
@@ -0,0 +1,28 @@
+/*
+* @Author: Administrator
+* @Date:   2018-04-11 20:43:41
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-04-11 21:27:26
+*/
+var button1=document.querySelector(".name");
+    button1.onclick=function(){
+       	alert("咱老李是谁啊！");
+}
+var button2=document.querySelector(".sex");
+    button2.onclick=function(){
+       	alert("二营长，你他娘的意大利炮呢！");
+}
+var button3=document.querySelector(".hobbit");
+    button3.onclick=function(){
+       	alert("～～秀芹大妹子～～");
+}
+var button4=document.querySelector(".idol");
+    button4.onclick=function(){
+       	alert("精锐？！老子打的就他娘的精锐！");
+}
+var button5=document.querySelector(".con");
+    button5.onclick=function(){
+       	alert("开炮！开炮！开………炮……");
+}
+
+         
